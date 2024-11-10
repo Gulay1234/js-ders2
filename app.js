b2f4efb2a848e077fecc7ce5b98b3224c381fdc5
Gulay <gulaynasibova28@gmail.com>
2024-11-10 12:03:30 +0400

    js-ders2
    js-ders2

---
+++ app.js
@@ -0,0 +1,14 @@
+let ad = prompt("Adinizi daxil edin!")
+console.log(ad)
+
+let yas = prompt("Yasinizi daxil edin!")
+console.log(yas)
+
+let sevimliReng = prompt("En sevdiyiniz rengi daxil edin!")
+console.log(sevimliReng)
+
+
+let sevdiyiFealiyyet = prompt("En sevdiyiniz fealliyeti daxil edin!")
+console.log(sevdiyiFealiyyet)
+
+document.write("Salam" + " " + ad + "!" + " " + "Senin" + " " + "yasin" + " " + yas + "," + " " + "Sevimli" + " " + "rengin" + " " + sevimliReng + "," + " " + "ve" + " " + "en" + " " + "sevdiyin" + " " + "fealiyyetin" + " " + sevdiyiFealiyyet)
